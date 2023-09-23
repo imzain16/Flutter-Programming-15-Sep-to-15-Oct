ed1048823de3428b4375422099af1f1821c3d86c
raozain16 <115299258+imzain16@users.noreply.github.com>
2023-09-23 21:18:31 +0500

    Add files via upload
    All About Flutter

---
+++ Flutter_Introduction_Sep21.dart
@@ -0,0 +1,55 @@
+		#Flutter Bacis/Sep-21
+
+1. Flutter is a Cross Platform that run on all platforms.
+It covers IOs, Dekstop, Android, Website by 1 Code.
+
+2. Cross Platfrom:  That works in multiple operating
+environments/Platforms. It's Independent. Write one
+code and reuse on different plateforms.
+
+3. Native vs Cross Plateform vs hybrid
+
+4. Cross Plateform - Flutter & React Native
+
+- Other Frameworks - IOs     - Andriod
+- RaisedButtom()   UIButton    Widget.Button
+- Flutter  don't need these it has it's own rendering
+engine to execute on different platforms.
+
+5. Flutter Benefits
+	- Increase Time
+	- Reusage
+	- Ability to go beyond Mobile
+	- Same Logic and UI in All Plateforms
+	- Custom, Animated UI of any complexity
+	- Similar to Native App Performance
+	- Own rendering Engine
+	- Simple Platform Specific Logic Implementation
+	- Flutter support Native UI
+	
+6. Flutter Arhitecture
+	- It's all about Widgets
+	- Have to create a widgte tree
+	- PLateform Differences
+	- One CodeBase (written in Dart)-(Both deisgning & Logic)
+	- Every Compenent is a widget
+	- Every Widget can has it's sub-widgets
+6.1 Widgtes: 
+	1. Widgets of Application (Every Page is a Widget)
+	2. Formation of Widget:
+		- MyApp
+		- MaterialApp
+		- MyHomePage
+		- Scaffold
+			- AppBar - text
+			- Body
+			- Floating Action Button - Navigation
+			Drawer - Bottom Navigation
+	3. Types
+		- Multiple
+		- Single
+
+		#Development Phase
+1. Flutter Setup
+2. Andriod Studio Installation
+3. VS Code Instalation
